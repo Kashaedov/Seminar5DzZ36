dd52b03b3fd433e67246814db6b0da5fe2809b7d
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-14 19:12:44 +0300

    Zadacha36

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿int size = 7; 
+int [] array = new int [size];
+
+
+void FillArray (int [] array)
+
+{
+    Random random = new Random ();
+    for (int i = 0; i < array.Length; i++)
+    {
+    array [i] = random.Next (1, 100);
+    }
+}
+
+void PrintArray (int [] array)
+{
+    Console.WriteLine ("Ввывод массива:");
+    for (int i = 0; i < array.Length; i++)
+    {
+    Console.Write (array[i] + "\t");
+    }
+   
+}
+
+void SumNeChetNumbers (int [] array)
+{
+    Console.WriteLine ();
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if (i % 2 == 1) sum += array[i];
+    }
+    Console.WriteLine ( $"Сумма элементов, стоящих на нечетных позициях = {sum}");
+}
+
+
+FillArray (array);
+PrintArray (array);
+SumNeChetNumbers (array);
